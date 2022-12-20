278bcb7ec7ba0aa23da119c9831b45ad0ff77716
Ali Sawari <ali.dragon.swr@gmail.com>
2022-12-20 22:24:25 +0300

    chore: adding an install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+zshrc="$HOME/.zshrc"
+bashrc="$HOME/.bashrc"
+
+gabriel="# gabriel\nsource $(pwd)/gabriel.sh"
+
+done_message="Done! Please restart your terminal to use gabriel"
+
+if [ -f $zshrc ]; then
+    echo "setting up gabriel for zsh";
+    echo -e $gabriel >> $zshrc
+    echo $done_message
+
+elif [ -f $bashrc ]; then
+    echo "setting up gabriel for bash";
+    echo -e $gabriel >> $bashrc
+    echo $done_message
+
+else
+    echo "Couldn't find a Shell!"
+fi
